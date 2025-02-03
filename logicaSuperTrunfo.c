0e9809be2fe1b490d0b4f38074030bdb79036668
GustaDias-dev <gustavodiassgd17@gmail.com>
2025-02-03 16:26:45 +0000

    Commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 
-int main (){
-
-    //CARTA1
-
-    //Variáveis
-    char estado;
-    char carta[20];
-    char cidade[20];
+int main () {
+    //Variaveis
+    char pais [20];
     int populacao;
     float area;
     float pib;
     int pturisticos;
-    float densidade_populacional;
-    float pib_capita;
-    
-
-    densidade_populacional = (float) (populacao / area);
-    pib_capita = (float) (pib / populacao);
-    
+    int escolha1;
     //Cadastro
-    printf("Digite o Estado: \n");
-    scanf("%c", &estado);
-    printf("Digite o código da carta: \n");
-    scanf("%s", &carta);
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade);
+    printf("Digite o Nome do País: \n");
+    scanf("%s", &pais);
     printf("Digite o número de habitantes: \n");
     scanf("%d", &populacao);
     printf("Digite a Área: \n");
@@ -34,34 +19,45 @@ int main (){
     scanf("%f", &pib);
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pturisticos);
-    printf("Densidade Populacional: %f pessoas/km²\n", densidade_populacional);
-    printf("PIB per capita: %f reais\n", pib_capita);
-    
-    
-    //CARTA2
 
-    //Variáveis
-    char estado2;
-    char carta2[20];
-    char cidade2[20];
+    printf("Escolha um atributo: \n");
+    printf("1. População.\n");
+    printf("2. Àrea.\n");
+    printf("3. PIB.\n");
+    printf("4. Pontos turísticos.\n");
+    printf("Escolha: ");
+    scanf("%d", &escolha1);
+
+    switch (escolha1)
+    {
+    case 1:
+    printf("O jogador 1 escolheu população\n");
+    break;
+    case 2:
+    printf("O jogador 1 escolheu Àrea\n");
+    break;
+    case 3:
+    printf("O jogador 1 escolheu PIb\n");
+    break;
+    case 4:
+    printf("O jogador 1 escolheu Pontos turísticos\n");
+    break;
+    default:
+    printf("Número inválido\n");
+    break;
+    }
+
+
+    //carta2
+    char pais2 [20];
     int populacao2;
     float area2;
     float pib2;
     int pturisticos2;
-    float densidade_populacional2;
-    float pib_capita2;
-   
-
-    densidade_populacional2 = (float) (populacao2 / area2);
-    pib_capita2 = (float) (pib2 / populacao2);
-    
+    int escolha2;
     //Cadastro
-    printf("Digite o Estado: \n");
-    scanf("%c", &estado2);
-    printf("Digite o código da carta: \n");
-    scanf("%s", &carta2);
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade2);
+    printf("Digite o Nome do País: \n");
+    scanf("%s", &pais2);
     printf("Digite o número de habitantes: \n");
     scanf("%d", &populacao2);
     printf("Digite a Área: \n");
@@ -70,17 +66,62 @@ int main (){
     scanf("%f", &pib2);
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pturisticos2);
-    printf("Densidade Populacional: %f pessoas/km²\n", densidade_populacional2);
-    printf("PIB per capita: %f reais\n", pib_capita2);
 
+    printf("Escolha um atributo: \n");
+    printf("1. População.\n");
+    printf("2. Àrea.\n");
+    printf("3. PIB.\n");
+    printf("4. Pontos turísticos.\n");
+    printf("Escolha: ");
+    scanf("%d", &escolha2);
+
+    switch (escolha2)
+    {
+    case 1:
+    printf("O jogador 1 escolheu população\n");
+    break;
+    case 2:
+    printf("O jogador 1 escolheu Àrea\n");
+    break;
+    case 3:
+    printf("O jogador 1 escolheu PIb\n");
+    break;
+    case 4:
+    printf("O jogador 1 escolheu Pontos turísticos\n");
+    break;
+    default:
+    printf("Número inválido\n");
+    break;
+    }
+
+    if (escolha1 > escolha2)
+    {
+        printf("Jogador 1 Venceu!\n");
+    } else if (escolha1 == escolha2){
+        printf("Empate\n");
+    } else {
+        printf("O jogador 2 Venceu!\n");
+    }
+
+    
+
+
+
+
+
+
+
+
+
+    
 
     //COMPARAÇÃO
-    if (pib > pib2)
+    /*if (pib > pib2)
     {
         printf("A Carta 1 é a Vencedora!!!\n");
     } else {
         printf("A carta 2 é a vencedora!!!\n");
-    }
+    }*/
     
     
 
